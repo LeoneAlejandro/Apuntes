477b168e0da1a425f2a236a05169c9d2db979a69
HUGO <ale.leone09@gmail.com>
2021-09-02 16:24:48 -0300

    02/09/2021

--- HackerRank/Day_14/src/com/leonealejandro/Main.java
+++ HackerRank/Day_14/src/com/leonealejandro/Main.java
@@ -1,8 +1,42 @@
 package com.leonealejandro;
 
+import java.util.Scanner;
+
+class Difference{
+    int[] _elements;
+    int maximumDifference;
+    int temp;
+
+    public Difference(int[] _elements) {
+        this._elements = _elements;
+    }
+
+    void computeDifference(){
+        for (int j = 0; j < _elements.length; j++) {
+            for (int i = 0; i < _elements.length - 1; i++) {
+                temp = Math.abs(_elements[j]) - Math.abs(_elements[i]);
+                if (Math.abs(temp) > maximumDifference)
+                    maximumDifference = Math.abs(temp);
+            }
+        }
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
     }
 }
