338a7792900d06ffef0fc45094417badbe1453f0
HUGO <ale.leone09@gmail.com>
2022-02-10 08:34:12 -0300

    10/02/2022

--- HackerRank/nonDivisibleSubset/src/com/leonealejandro/Main.java
+++ HackerRank/nonDivisibleSubset/src/com/leonealejandro/Main.java
@@ -1,19 +1,40 @@
 package com.leonealejandro;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
+    static int nonDivisibleSubset(int k, int[] S) {
+        int[] remainderArr = new int[k];
+        for (Integer each : S) {
+            remainderArr[each % k]++;
+        }
+        int zeroRemainder = remainderArr[0];
+        int maxNumberOfDivisibleSet = zeroRemainder > 0 ? 1 : 0;
+        for (int i = 1; i <= (k / 2); i++) {
+            if (i != k - i)
+                maxNumberOfDivisibleSet += Math.max(remainderArr[i], remainderArr[k - i]);
+            else
+                maxNumberOfDivisibleSet ++;
+        }
+        return maxNumberOfDivisibleSet;
+    }
+
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        List<Integer> s = new ArrayList<>();
+        int[] S = new int[n];
         for (int i = 0; i < n; i++) {
-            s.add(sc.nextInt());
+            S[i] = sc.nextInt();
         }
+        System.out.println(nonDivisibleSubset(k,S));
+
 
     }
 }
