a78b62339f0d32ee50c1898695cc7ddf8f8ed15b
HUGO <ale.leone09@gmail.com>
2022-06-09 14:54:25 -0300

    09/06/2022

--- HackerRank/Flatland Space Stations/src/com/leonealejandro/Main.java
+++ HackerRank/Flatland Space Stations/src/com/leonealejandro/Main.java
@@ -1,29 +1,27 @@
 package com.leonealejandro;
 
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    int cities = sc.nextInt();
-        int n = sc.nextInt(), dist = 0, maxdif = 0;
+        Scanner sc = new Scanner(System.in);
+        int cities = sc.nextInt();
+        int n = sc.nextInt();
         int[] ss = new int[n];
         for (int i = 0; i < n; i++) {
             ss[i] = sc.nextInt();
         }
-        for (int i = 0; i < cities; i++) {
-            for (int j = 0; j < n; j++) {
-                if (cities == ss[j]){
-                    if (maxdif<dist) maxdif=dist;
-                    dist = 0;
-                }dist++;
-            }
-        }
-        System.out.println(maxdif);
-        }
+        List<Integer> Distancias = new ArrayList<>();
+        Arrays.sort(ss);
+        Distancias.add(ss[0]);
 
-
+        for (int i = 1; i < n; i++) {
+            Distancias.add((ss[i]-ss[i-1])/2);
+        }
+        Distancias.add(cities-1-ss[n-1]);
+        System.out.println(Distancias);
+        System.out.println(Collections.max(Distancias));
+    }
 }
